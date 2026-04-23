0281809c52aa2a7fc0833ba0d65efc69dea31370
unknown <sousadiego533@gmail.com>
2026-04-23 20:22:26 -0300

    test: add EntityTest

---
+++ backend/test/core/shared/Entity.test.ts
@@ -0,0 +1,37 @@
+import Entity from "@/core/shared/Entity"
+
+export interface TestProps {
+    id?: string
+    name?: string
+    idade?: number
+}
+
+class Test extends Entity<Test, TestProps> {
+    props: TestProps
+
+    constructor(props: TestProps) {
+        super(props)
+        this.props = props
+    }
+}
+
+test('Deve comparar duas entidades iguais', ()=>{
+    const t1 = new Test({name: 'teste 1', idade: 16})
+    const t2 = t1.clone()
+    expect(true).toBe(t2.equals(t1))
+    expect(false).toBe(t2.diferent(t1))
+})
+
+test('Deve comparar duas entidades diferentes', ()=>{
+    const t1 = new Test({name: 'teste 1', idade: 16})
+    const t2 = new Test({name: 'teste 2', idade: 17})
+    expect(false).toBe(t1.equals(t2))
+    expect(true).toBe(t1.diferent(t2))
+})
+
+test('Deve clonar uma entidade', ()=>{
+    const t1 = new Test({name: 'teste 1', idade: 16})
+    const t2 = t1.clone()
+    expect(true).toBe(t1.equals(t2))
+})
+
